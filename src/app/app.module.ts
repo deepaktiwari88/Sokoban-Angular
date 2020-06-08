a1fca4dcadea2d273cd62fd1ddb1db5f7224c459
Deepak Tiwari <tiwarid99@gmail.com>
2020-06-08 20:17:34 +0530

    Added HomeComponent in Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { LevelsComponent } from "./levels/levels.component";
 import { AppRoutingModule } from "./app-routing/app-routing.module";
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
-  declarations: [AppComponent, LevelsComponent],
+  declarations: [AppComponent, LevelsComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
