22b8d92d18068aa0faef799f2b7904cabede7e49
Madhav Pruthi <madhavpruthi.becse16@pec.edu.in>
2020-06-08 20:40:55 +0530

    Updated Version of Packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { AppComponent } from "./app.component";
 import { LevelsComponent } from "./levels/levels.component";
 import { AppRoutingModule } from "./app-routing/app-routing.module";
 import { HomeComponent } from "./home/home.component";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [AppComponent, LevelsComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
