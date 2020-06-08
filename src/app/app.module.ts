2da643bb465f507a5cca70cc5f28c4a54c02e55b
Madhav Pruthi <madhavpruthi.becse16@pec.edu.in>
2020-06-08 20:16:41 +0530

    Added Material UI Layout CDK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { LevelsComponent } from "./levels/levels.component";
 import { AppRoutingModule } from "./app-routing/app-routing.module";
+import { HomeComponent } from "./home/home.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, LevelsComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, LevelsComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
