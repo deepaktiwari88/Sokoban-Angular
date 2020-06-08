9179588e3c6f011e871c0360f70f797e54e109b1
Arunav Sharma <arunavsharma11@gmail.com>
2020-06-09 00:23:40 +0530

    Movement added

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {COLORS} from './constants';
+import {COLORS,BOARD_SIZE} from './constants';
+import { BROWSER_ANIMATIONS_PROVIDERS } from '@angular/platform-browser/animations/src/providers';
 
 @Component({
   selector: 'app-canvas',
@@ -7,8 +8,12 @@ import {COLORS} from './constants';
   styleUrls: ['./canvas.component.scss']
 })
 export class CanvasComponent implements OnInit {
-public rows:number=5;
+
 public board=[];
+public x=0;
+public y=0;
+public a=2;
+public b=1;
   constructor() { }
 
   ngOnInit() {
@@ -17,17 +22,51 @@ public board=[];
   setBoard(): void {
     this.board = [];
 
-    for (let i = 0; i < this.rows; i++) {
+    for (let i = 0; i < BOARD_SIZE; i++) {
       this.board[i] = [];
-      for (let j = 0; j < this.rows; j++) {
+      for (let j = 0; j <BOARD_SIZE; j++) {
         this.board[i][j] = false;
       }
     }
+    this.board[0][0]=true;
+    this.board[this.a][this.b]=true;
 }
 setColors(col: number, row: number): string {
- 
-
+  if(this.board[col][row] === true && col===this.a && row ===this.b)
+  
+    return COLORS.BOX;
+  
+ else if (this.board[col][row] === true) {
+    return COLORS.BODY;}
+    if(this.board[col][row] === true && col==this.a && row ==this.b)
+    {
+      return COLORS.BOX;
+    }
   return COLORS.BOARD;
 };
-
+moveLeft()
+{this.board[this.x][this.y]=false;
+  this.x=this.x-1;
+  this.board[this.x][this.y]=true;
+  this.setColors(this.y,this.x);
+};
+moveRight()
+{
+  this.board[this.x][this.y]=false;
+  this.x=this.x+1;
+  this.board[this.x][this.y]=true;
+  this.setColors(this.y,this.x);
+};
+moveUp()
+{this.board[this.x][this.y]=false;
+  this.y=this.y-1;
+  this.board[this.x][this.y]=true;
+  this.setColors(this.y,this.x);
+};
+moveDown()
+{this.board[this.x][this.y]=false;
+  this.y=this.y+1;
+  this.board[this.x][this.y]=true;
+  this.setColors(this.y,this.x);
+};
 }
--- src/app/canvas/constants.ts
+++ src/app/canvas/constants.ts
@@ -1,6 +1,10 @@
+
+export const BOARD_SIZE = 5;
 export const COLORS = {
    
     
-    BOARD: '#FFFFFF',
+    BOARD: '#86B5BD',
+    BODY: '#FFFFFF',
+    BOX:'#000000',
     
   };
